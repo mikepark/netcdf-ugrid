19c16b94182b1c5d5966d2f23ef224e39b27f8bb
Mike Park <Mike.Park@NASA.Gov>
2011-03-14 13:33:00 +0100

    nc_open with try error handling

--- netcdf-ugrid.c
+++ netcdf-ugrid.c
@@ -3,8 +3,28 @@
 #include <stdio.h>
 #include <netcdf.h>
 
+#define nc_try( fcn )						\
+  {								\
+    int code;							\
+    code = (fcn);						\
+    if ( code )							\
+      {								\
+	printf("netcdf error: %s\n", nc_strerror(code));	\
+	exit(code);						\
+      }								\
+  }
+
 int main( int argc, char *argv[] )
 {
+  int nc_handle;
+
+  if ( argc < 2 ) 
+    {
+      printf("usage: %s my-netcfd-grid-filename\n",argv[0]);
+      return 1;
+    }
+
+  nc_try( nc_open(argv[1], NC_NOWRITE, &nc_handle) );
 
   return 0;
 }
