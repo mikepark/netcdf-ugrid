fb429adcafa25650638162186d907836b0fc0aec
Mike Park <Mike.Park@NASA.Gov>
2011-03-14 14:01:33 +0100

    to get the dimensions of the hexs

--- netcdf-ugrid.c
+++ netcdf-ugrid.c
@@ -17,6 +17,11 @@
 int main( int argc, char *argv[] )
 {
   int nc_handle;
+  int dim_handle;
+  size_t dim_length;
+
+  int nhex;
+  int nodes_per_hex;
 
   if ( argc < 2 ) 
     {
@@ -25,6 +30,18 @@ int main( int argc, char *argv[] )
     }
 
   nc_try( nc_open(argv[1], NC_NOWRITE, &nc_handle) );
+
+  nc_try( nc_inq_dimid(nc_handle, "no_of_hexaeders", &dim_handle) );
+  nc_try( nc_inq_dimlen(nc_handle, dim_handle, &dim_length) );
+  nhex = (int)dim_length;
+
+  printf("nhex %d\n",nhex);
+
+  nc_try( nc_inq_dimid(nc_handle, "points_per_hexaeder", &dim_handle) );
+  nc_try( nc_inq_dimlen(nc_handle, dim_handle, &dim_length) );
+  nodes_per_hex = (int)dim_length;
+
+  printf("nodes_per_hex %d\n",nodes_per_hex);
 
   return 0;
 }
